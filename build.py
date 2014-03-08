60bacfa6673a4e0a8f2716e2026f59d423a736ed
Jonn Mostovoy <amarr.industrial@gmail.com>
2014-03-08 02:09:01 +0100

    Wtf did I just do

---
+++ build.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import json
+import os
+from subprocess import call
+
+with open('dub.json', 'r') as f:
+    dub = f.read()
+
+dub = json.loads(dub)
+
+packages = [ dub["name"] ]
+for sub in dub["subPackages"]:
+    packages.append(packages[0] + ":" + sub["name"])
+
+for package in packages:
+    call(["dub", "build", package])
+    
+os.system('mv -vt bin/ nym* *.a')
+#call(["mv", "-vt", "bin/", "nym*", "*.a"])
