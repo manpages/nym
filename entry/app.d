8e06ea633c9fca59d993ccbce48b4cba4a78958b
Jonn Mostovoy <amarr.industrial@gmail.com>
2014-03-08 05:35:53 +0100

    Set up timeout in simple REP/REQ.

--- entry/app.d
+++ entry/app.d
@@ -2,6 +2,7 @@ import std.stdio;
 import std.exception;
 import std.conv;
 import std.string;
+import core.stdc.errno;
 import deimos.zmq.zmq;
 
 int main(string args[]) {
@@ -13,22 +14,24 @@ int main(string args[]) {
   foreach(arg; args[2 .. $]) {
     data ~= (' ' ~ arg);
   }
-  writeln(`We want to send "`, data, `" of length `, data.length);
-  //
   // Initialize zmq and zmq socket
   writeln(`Initializing zmq socket`);
   void* context = zmq_init(1);
   void* socket  = zmq_socket(context, ZMQ_REQ);
+
   writeln(`Done.`);
   writeln(`Connecting to zmq server`);
   // Connect to zmq server, mutates socket!
   zmq_connect(socket, toStringz(`tcp://localhost:67831`));
   writeln(`Done.`);
 
+  int timeout = 2500;
+  zmq_setsockopt(socket, ZMQ_SNDTIMEO, &timeout, timeout.sizeof);
+  zmq_setsockopt(socket, ZMQ_RCVTIMEO, &timeout, timeout.sizeof);
+
   writeln(`Preparing request`);
   // Prepare mutable request variable
   zmq_msg_t msg;
-  //zmq_msg_init_data(&msg, cast(void*)data.ptr, data.length);
   zmq_msg_init_size(&msg, data.length);
   // memcpy via slicing
   (zmq_msg_data(&msg))[0 .. data.length] = (cast(immutable(void*))data.ptr)[0 .. data.length];
@@ -42,27 +45,30 @@ int main(string args[]) {
    ***  1
    *** The more you learn.
    ***/
-  foreach(i; 0 .. data.length) {
-    write((cast(char*)zmq_msg_data(&msg))[i]);
-  }
   writeln("");
   writeln(`Done.`);
   writeln(`Sending request`);
   zmq_sendmsg(socket, &msg, 0);
-  zmq_msg_close(&msg);
   writeln(`Done.`);
 
   writeln(`Getting reply`);
   zmq_msg_t reply;
   zmq_msg_init(&reply);
-  ulong bytes = zmq_recvmsg(socket, &reply, 0);
-  writeln(`Reply length: `, to!string(bytes));
+  long bytes = zmq_recvmsg(socket, &reply, 0);
+  if(bytes != -1) {
+    writeln(`Reply length: `, to!string(bytes));
+  } else {
+    writeln(to!string(cast(char*)zmq_strerror(errno)));
+  }
   zmq_msg_close(&reply);
   writeln(`Done.`);
 
   // That's it for Zmq
   zmq_close(socket);
-  zmq_term(context);
+  writeln(`Closed socket.`);
+  //term never returns the control but why do we care, the process will terminate anyway.
+  //zmq_term(context);
+  //writeln(`Terminated context.`);
 
   writeln(`Pretty printing reply`);
   writeln(`:(`);
