5e32cffba4581d87bf7eee3d38c00d9cc5de9ac0
Jonn Mostovoy <amarr.industrial@gmail.com>
2014-03-13 10:10:21 +0100

    Implement core.lib.rpc_info.

--- Nym/core/lib.d
+++ Nym/core/lib.d
@@ -9,7 +9,7 @@ response rpc_add(string[] args, state nym_state) @safe pure {
   if(args.length < 1) {
     return tuple("Add expects an argument.", cast(state)null);
   }
-  if(string root = args[0].maybe_find_handle(nym_state)) {
+  if(string root = args[0].maybe_known(nym_state)) {
     return tuple(args[0] ~ " is already in the local database under main handle " ~ root, cast(state)null);
   }
   state result;
@@ -24,7 +24,7 @@ response rpc_alias(string[] args, state nym_state) @safe pure {
   if(!(args[0] in nym_state)) {
     return tuple("Main handle " ~args[0] ~ " isn't in the local database.", cast(state)null);
   }
-  if(string root = args[1].maybe_find_handle(nym_state)) {
+  if(string root = args[1].maybe_known(nym_state)) {
     return tuple(args[1] ~ " is already in the local database under main handle " ~ root, cast(state)null);
   }
   state result;
@@ -32,8 +32,19 @@ response rpc_alias(string[] args, state nym_state) @safe pure {
   return tuple("ok", result);
 }
 
-response rpc_info(string[] _nothing, state nym_state) @safe pure {
-  return tuple("Not implemented", cast(state)null);
+response rpc_info(string[] args, state nym_state) @safe pure {
+  if(args.length < 3) {
+    return tuple("Info expects at least three arguments.", cast(state)null);
+  }
+  if(!(args[0] in nym_state)) {
+    return tuple("Main handle " ~args[0] ~ " isn't in the local database.", cast(state)null);
+  }
+  if((args[1] in nym_state[args[0]]) && (in_array(args[2], nym_state[args[0]][args[1]]))) {
+    return tuple("Main handle " ~ args[2] ~ " isn't already there.", cast(state)null);
+  }
+  state result;
+  result[args[0]] = [args[1]: [args[2]]];
+  return tuple("ok", result);
 }
 
 response rpc_get(string[] _nothing, state nym_state) @safe pure {
@@ -48,14 +59,25 @@ response rpc_default(string[] _nothing) @safe pure {
   return tuple("Not implemented", cast(state)null);
 }
 
-string maybe_find_handle(string who, state where) @safe pure {
-  if(who in where)
-    return who;
-  foreach(k, v; where) {
-    foreach(handle; v["handles"]) {
-      if(who == handle)
+string maybe_known(string who, state where) @safe pure {
+  return maybe_in("handles", who, where);
+}
+string maybe_in(string field, string needle, state haystack) @safe pure {
+  if(needle in haystack)
+    return needle;
+  foreach(k, v; haystack) {
+    foreach(handle; v[field]) {
+      if(needle == handle)
         return k;
     }
   }
   return cast(string)null;
+}
+
+bool in_array(T)(T needle, T[] haystack) {
+  foreach(v; haystack) {
+    if(needle == v)
+      return true;
+  }
+  return false;
 }
